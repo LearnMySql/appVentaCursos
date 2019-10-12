e4c1599efc80aa96cdd5e78cd18f04dc2844e1aa
JesusCanales <jesuscanales@outlook.com>
2019-10-11 19:15:46 -0500

    crear schema de base de datos

---
+++ SCHEMA/create_schema.sql
@@ -0,0 +1,9 @@
+/* Crear esquema */
+CREATE SCHEMA IF NOT EXISTS VENTA_CURSO
+DEFAULT CHARACTER SET utf8;
+
+/* Poner en uso el esquema de base de datos */
+USE VENTA_CURSO;
+
+/* Ver esquema activo */
+SELECT database();
